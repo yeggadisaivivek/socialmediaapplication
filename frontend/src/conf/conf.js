d92fa5cdc746d93834449a63568a0bbaf97da4c8
Vivek <yeggadisaivivek@gmail.com>
2024-07-31 16:14:20 -0500

    made changes

--- frontend/src/conf/conf.js
+++
@@ -1,9 +0,0 @@
-const conf = {
-    appwriteURL:process.env.REACT_APP_APPWRITE_URL,
-    appwriteProjectId:process.env.REACT_APP_APPWRITE_PROJECT_ID,
-    appwritedatabaseId:process.env.REACT_APP_APPWRITE_DATABASE_ID,
-    appwriteCollectionUsersId:process.env.REACT_APP_APPWRITE_COLLECTION_USERS_ID,
-    appwriteBucketId:process.env.REACT_APP_APPWRITE_BUCKET_ID
-}
-
-export default conf;
